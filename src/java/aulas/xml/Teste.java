030ef7ef94de049c4fb4fe2744e20618ff5418d5
Hugo Mascherini Fioranelli <hugo.mascherini29@gmail.com>
2025-08-21 11:39:37 -0300

    Criacao  de biblioteca logtrack

---
+++ src/java/aulas/xml/Teste.java
@@ -0,0 +1,11 @@
+package aulas.xml;
+
+import logtrack.ExceptionLogTrack;
+
+public class Teste {
+    public static void main(String[] args) {
+        
+        ExceptionLogTrack.getInstance().addLog( new Exception("teste2"));
+        
+    }
+}
